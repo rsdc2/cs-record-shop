482bc84417028152075805e23ea8a42fa777deec
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-14 10:49:54 +0000

    Add minimal test for albums

--- RecordShop_Tests/ModelTests/AlbumsModelTests.cs
+++ RecordShop_Tests/ModelTests/AlbumsModelTests.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RecordShop;
+using FluentAssertions;
 
 namespace RecordShop_Tests.ModelTests
 {
     internal class AlbumsModelTests
     {
+        AlbumsModel _model;
+
+        private static Album greatAlbum = new Album(id: 1, title: "Great album", artist: "Great artist");
+        private static List<Album> testAlbums = new()
+        {
+            greatAlbum
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+            _model = new AlbumsModel();
+
+        }
+
+        [Test]
+        public void GetAllAlbumsReturnsListOfAlbums()
+        {
+            // Arrange
+            
+
+            // Act
+            var albums = _model.GetAllAlbums();
+
+            // Assert
+            albums.Should().NotBeNull();
+        }
     }
 }
