23c410949d7a94f63e71a7f0149067f3d4e9ea98
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-14 10:24:47 +0000

    Implement minimal album model

---
+++ RecordShop/Models/AlbumsModel.cs
@@ -0,0 +1,22 @@
+﻿namespace RecordShop
+{
+    
+
+    public interface IAlbumsModel
+    {
+        List<Album> GetAllAlbums();
+    }
+
+    public class AlbumsModel
+    {
+        private static Album greatAlbum = new Album(id: 1, title: "Great album", artist: "Great artist");
+        private static List<Album> testAlbums = new()
+        {
+            greatAlbum
+        };
+        public List<Album> GetAllAlbums()
+        {
+            return testAlbums;
+        }
+    }
+}
