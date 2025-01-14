87abadffd99121b09127b88e0427bc56b0ee754c
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-14 10:08:00 +0000

    Implement health controller

---
+++ RecordShop/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecordShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        public HealthController() { }
+
+        [HttpGet]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok("Server is running");
+        }
+    }
+}
